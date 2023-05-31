746b2995880cd6f65cfc73606be7e9a94284098a
Nematullo <nematullo@MacBook-Pro.local>
2023-06-01 01:42:56 +0500

    17-dars

---
+++ sass/js/main.js
@@ -0,0 +1,37 @@
+
+const tabs = document.querySelectorAll(".ap-tab-link");
+const tabs_content = document.querySelectorAll(".ap-tab-content");
+
+function getActiveTab(i) {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active-tab");
+  });
+  tabs_content.forEach((tabContent) => {
+    tabContent.classList.remove("active-tab");
+    tabContent.style.display = "none";
+  });
+
+  const selectedTab = tabs[i];
+  const selectedTabContent = tabs_content[i];
+
+  selectedTab.classList.add("active-tab");
+  selectedTabContent.classList.add("active-tab");
+  selectedTabContent.style.display = "block";
+}
+
+getActiveTab(0);
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    getActiveTab(i);
+  });
+}
+
+const showImg = document.querySelector("#show img");
+const indicators = document.querySelector(".indicators");
+
+indicators.addEventListener("click", function (e) {
+    e.target.src && (showImg.src = e.target.src);
+});
+
